5a804ccc31a992371d7fb85c0fb119f97e8bd326
jgyerjabin <T5z6u7i8>
2021-07-07 15:02:01 +0200

    x

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'stopper-app';
+  counter: number = 0;
+  interval;
+  timeStepList: Array<number> = []; 
+  isStarted: boolean = false;
+
+  startPauseStopper() {
+    if (!this.isStarted){
+      this.interval = setInterval(() => {
+        if (this.counter === 0) {
+          this.counter++;
+        } else {
+          this.counter++;
+        }
+    }, 0.1);
+      this.isStarted= true;
+    } else {
+      clearInterval(this.interval);
+      this.isStarted= false;
+    }
+    
+
+  }
+  pauseTimer() {
+    clearInterval(this.interval);
+    this.isStarted= false;
+  }
+
+  resetStopper(){
+    this.counter=0;
+  }
+  addStep(){
+    this.timeStepList.push(this.counter);
+  }
 }
